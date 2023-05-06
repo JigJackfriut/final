d7afaafb75f029e0540184bde7c612248b7962fb
JigJackfriut <124086458+JigJackfriut@users.noreply.github.com>
2023-05-06 16:41:32 -0400

    Update dynamicArt.js

--- dynamicArt.js
+++ dynamicArt.js
@@ -1,56 +1,60 @@
-var baseUrl = 'http://3.15.139.27:5005';
-const url = "http://localhost:5005/dynamicArt/getEmotionPercentages";
+const baseUrl = 'http://3.15.139.27:5005';
+const url = baseUrl + '/dynamicArt/getEmotionPercentages/';
 
 window.onload = function() {
-    const baseUrl = "http://3.15.139.27:5005";
+  const canvas = document.getElementById('word-cloud');
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
 
-    const canvas = document.getElementById("word-cloud");
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+  if (canvas.getContext) {
+    const ctx = canvas.getContext('2d');
 
-    if (canvas.getContext) {
-        const ctx = canvas.getContext("2d");
-
-        fetchData().then(emotionData => {
-            createWordCloud(emotionData, ctx);
-        });
-    }
+    fetchData().then(emotionData => {
+      const words = createWordData(emotionData);
+      createWordCloud(words, ctx);
+    });
+  }
 };
 
 async function fetchData() {
-    const response = await fetch("http://3.15.139.27:5005/retrieve/words");
-    const data = await response.json();
-    return data.words;
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
 }
 
-function createWordCloud(emotionData, ctx) {
-    const words = emotionData.map(word => {
-        return {
-            text: word,
-            size: Math.random() * 50 + 20,
-            color: getRandomColor(),
-            x: Math.random() * ctx.canvas.width,
-            y: Math.random() * ctx.canvas.height,
-            speed: Math.random() * 3 + 1
-        };
-    });
+function createWordData(emotionData) {
+  const words = [];
+  for (const emotion in emotionData) {
+    const count = emotionData[emotion];
+    for (let i = 0; i < count; i++) {
+      words.push(emotion);
+    }
+  }
+  return words;
+}
 
-    setInterval(() => {
-        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-        words.forEach(word => {
-            ctx.font = `${word.size}px Arial`;
-            ctx.fillStyle = word.color;
-            ctx.fillText(word.text, word.x, word.y);
-            word.x += word.speed;
-            if (word.x > ctx.canvas.width) {
-                word.x = -ctx.measureText(word.text).width;
-                word.y = Math.random() * ctx.canvas.height;
-            }
-        });
-    }, 24);
+function createWordCloud(words, ctx) {
+  const cloud = d3.layout.cloud().size([ctx.canvas.width, ctx.canvas.height])
+    .words(words.map(function(d) { return {text: d, size: Math.random() * 50 + 20}; }))
+    .padding(5)
+    .rotate(function() { return ~~(Math.random() * 2) * 90; })
+    .fontSize(function(d) { return d.size; })
+    .on('end', draw);
+
+  cloud.start();
+
+  function draw(words) {
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    words.forEach(function(d) {
+      ctx.font = d.size + 'px Arial';
+      ctx.fillStyle = getRandomColor();
+      ctx.fillText(d.text, d.x, d.y);
+    });
+  }
 }
 
 function getRandomColor() {
-    const colors = ["#648FFF", "#785EF0", "#DC267F", "#FE6100", "#FFB000"];
-    return colors[Math.floor(Math.random() * colors.length)];
+  const colors = ['#648FFF', '#785EF0', '#DC267F', '#FE6100', '#FFB000'];
+  return colors[Math.floor(Math.random() * colors.length)];
 }
+
