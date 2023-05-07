bf003e2cde92174ea53a101af8bfdb3244e9b6a0
JigJackfriut <124086458+JigJackfriut@users.noreply.github.com>
2023-05-06 21:50:02 -0400

    Update dynamicArt.js

--- dynamicArt.js
+++ dynamicArt.js
@@ -9,10 +9,30 @@ window.onload = function() {
   if (canvas.getContext) {
     const ctx = canvas.getContext('2d');
 
-    fetchData().then(emotionData => {
-      const words = createWordData(emotionData);
-      createWordCloud(words, ctx);
-    });
+    setInterval(() => {
+      fetchData().then(emotionData => {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        const words = Object.keys(emotionData);
+        const maxCount = Math.max(...Object.values(emotionData));
+        const minSize = 30;
+        const maxSize = 80;
+        const height = canvas.height / (words.length + 1);
+        const fontSizeFactor = (maxSize - minSize) / maxCount;
+
+        for (let i = 0; i < words.length; i++) {
+          const word = words[i];
+          const count = emotionData[word];
+          let size = minSize + count * fontSizeFactor;
+          let x = canvas.width / 2;
+          let y = (i + 1) * height;
+          ctx.font = `${size}px Arial`;
+          ctx.fillStyle = getRandomColor();
+          ctx.textAlign = 'center';
+          ctx.fillText(word, x, y);
+        }
+      });
+    }, 1000);
   }
 };
 
@@ -22,39 +42,9 @@ async function fetchData() {
   return data;
 }
 
-function createWordData(emotionData) {
-  const words = [];
-  for (const emotion in emotionData) {
-    const count = emotionData[emotion];
-    for (let i = 0; i < count; i++) {
-      words.push(emotion);
-    }
-  }
-  return words;
-}
 
-function createWordCloud(words, ctx) {
-  const cloud = d3.layout.cloud().size([ctx.canvas.width, ctx.canvas.height])
-    .words(words.map(function(d) { return {text: d, size: Math.random() * 50 + 20}; }))
-    .padding(5)
-    .rotate(function() { return ~~(Math.random() * 2) * 90; })
-    .fontSize(function(d) { return d.size; })
-    .on('end', draw);
-
-  cloud.start();
-
-  function draw(words) {
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-    words.forEach(function(d) {
-      ctx.font = d.size + 'px Arial';
-      ctx.fillStyle = getRandomColor();
-      ctx.fillText(d.text, d.x, d.y);
-    });
-  }
-}
 
 function getRandomColor() {
   const colors = ['#648FFF', '#785EF0', '#DC267F', '#FE6100', '#FFB000'];
   return colors[Math.floor(Math.random() * colors.length)];
 }
-
