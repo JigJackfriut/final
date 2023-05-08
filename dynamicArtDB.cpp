2aae82d63bb3d446313cfb4719c83a143aa63345
JigJackfriut <124086458+JigJackfriut@users.noreply.github.com>
2023-05-08 00:41:42 -0400

    Update dynamicArtDB.cpp

--- dynamicArtDB.cpp
+++ dynamicArtDB.cpp
@@ -6,64 +6,49 @@
 
 
 dynamicArtDB::dynamicArtDB() {
-  	// Instantiate Driver
-  	driver = sql::mariadb::get_driver_instance();
-  	
- 	// Configure Connection
-  	// The URL or TCP connection string format is
-  	// ``jdbc:mariadb://host:port/database``.
-  	sql::SQLString url(db_url);
+        // Instantiate Driver
+        driver = sql::mariadb::get_driver_instance();
+
+        // Configure Connection
+        // The URL or TCP connection string format is
+        // ``jdbc:mariadb://host:port/database``.
+        sql::SQLString url(db_url);
 
     // Use a properties map for the other connection options
-  	sql::Properties my_properties({{"user", user}, {"password",pass}});
-  	// Save properties in object
-  	properties = my_properties;
+        sql::Properties my_properties({{"user", user}, {"password",pass}});
+        // Save properties in object
+        properties = my_properties;
 
     // Establish Connection
-  	unique_ptr<sql::Connection> my_conn(driver->connect(db_url, properties));
+        unique_ptr<sql::Connection> my_conn(driver->connect(db_url, properties));
     
     // Check success
     if (!my_conn) {
-   		cerr << "Invalid database connection" << endl;
-   		exit (EXIT_FAILURE);
-   	}	
-   	
-   	// Save connection in object
-   	conn = std::move(my_conn);
-   	
+                cerr << "Invalid database connection" << endl;
+                exit (EXIT_FAILURE);
+        }
+
+        // Save connection in object
+        conn = std::move(my_conn);
+
 }
 
-// Calculates percentage of emotions 
-void dynamicArtDB::getEmotionCount() {
-    if (!conn) {
-        cerr << "Invalid database connection" << endl;
-        exit(EXIT_FAILURE);
+map<string, int> dynamicArtDB::getEmotionCount() {
+    std::map<std::string, int> emotionCounts;
+
+// List of emotions to count
+std::vector<std::string> emotions = {"Happiness", "Fear", "Sadness", "Anger"};
+
+// Iterate over the list of emotions and count each one
+for (const auto& emotion : emotions) {
+    std::string query = "SELECT COUNT(*) AS count FROM my_table WHERE emotions = ?";
+    sql::PreparedStatement *stmt = conn->prepareStatement(query);
+    stmt->setString(1, emotion);
+    sql::ResultSet *res = stmt->executeQuery();
+    if (res->next()) {
+        int count = res->getInt("count");
+        emotionCounts[emotion] = count;
     }
-
-    unique_ptr<sql::Statement> stmnt(conn->createStatement());
-    unique_ptr<sql::ResultSet> resTotal(stmnt->executeQuery("SELECT COUNT(*) as total FROM responses"));
-    int totalResponses = 0;
-    if (resTotal->next()) {
-        totalResponses = resTotal->getInt("total");
-    }
-
-    map<string, int> emotionCounts;
-    unique_ptr<sql::ResultSet> resEmotions(stmnt->executeQuery("SELECT emotion, COUNT(*) as count FROM responses GROUP BY emotion"));
-
-    while (resEmotions->next()) {
-        string currentEmotion = resEmotions->getString("emotion");
-        int count = resEmotions->getInt("count");
-        emotionCounts[currentEmotion] = count;
-    }
-
-    map<string, double> emotionPercentages;
-    for (const auto &entry : emotionCounts) {
-        emotionPercentages[entry.first] = static_cast<double>(entry.second) / totalResponses * 100;
-    }
-
-    // emotionPercentages now contains the percentage of responses for each emotion
-    // You can return it or use it as needed for the word cloud
-  return emotionPercentages;
-      
 }
-
+  return emotionCounts;
+}
